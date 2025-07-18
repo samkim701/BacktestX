6d4daba896660c71b9d750d5139ef944c42a82b9
Sam Kim <117691164+samkim701@users.noreply.github.com>
2025-07-18 21:26:04 +0000

    Added Buy Hold Return

--- backtester.py
+++ backtester.py
@@ -22,18 +22,22 @@ def add_signals(df, short_window=10, long_window=50):
 
 ##Actual backtest
 def backtest(df):
-    df['Strategy Return'] = df['Position'] * df['Return'] # Remember, the return could be negative when we have position
-    df['Cum_Strategy'] = (1 + df['Strategy Return']).cumprod() #cumprod is cumulative product. Ex: [1,2,3,4] = [1,2,6,24] 
+    df['Strategy_Return'] = df['Position'] * df['Return'] # Remember, the return could be negative when we have position
+    df['Cum_Strategy'] = (1 + df['Strategy_Return']).cumprod() #cumprod is cumulative product. Ex: [1,2,3,4] = [1,2,6,24] 
     df['Cum_Buy_Hold'] = (1 + df['Return']).cumprod()
-    total_return = df['Cum_Strategy'].iloc[-1] - 1
+    total_strategy_return = df['Cum_Strategy'].iloc[-1] - 1
+    total_buy_hold_return = df['Cum_Buy_Hold'].iloc[-1] - 1
     cagr = (df['Cum_Strategy'].iloc[-1]) ** (252 / len(df)) - 1 #I can only trade 252 days a year
     sharpe = (df['Strategy_Return'].mean() / df['Strategy_Return'].std()) * np.sqrt(252) #Sharpe Ratio (Standard version of the risk-return ratio)
     max_drawdown = (df['Cum_Strategy'] / df['Cum_Strategy'].cummax() - 1).min() #Shows the worst pain an investor would experience, cummax() : gives the running maximum value 
-    print(f"Total Return: {total_return:.2%}")
+    print(f"Total Strategy Return: {total_strategy_return:.2%}")
+    print(f"Total Buy Hold Return: {total_buy_hold_return:.2%}")
     print(f"CAGR: {cagr:.2%}")
     print(f"Sharpe Ratio: {sharpe:.2f}")
     print(f"Max Drawdown: {max_drawdown:.2%}")
     
     return df
+
+backtest(add_signals(download_data("AAPL", "2018-01-01", "2024-01-01")))
 
 
