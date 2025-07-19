826e809999821b1852363312ed520867afc3f772
Sam Kim <117691164+samkim701@users.noreply.github.com>
2025-07-19 02:02:43 +0000

    backtester logic done

--- backtester.py
+++ backtester.py
@@ -2,8 +2,9 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime
 
-#Load stock data
+## Load stock data
 def download_data(ticker, start, end):
     df = yf.download(ticker, start=start, end=end) #open, high, low, close, adj close, volume
     df['Return'] = df['Close'].pct_change()
@@ -18,9 +19,9 @@ def add_signals(df, short_window=10, long_window=50):
     df['Position'] = df['Signal'].shift(1) #Realistic buy, you won't be able to buy right away
     return df
 
-##print(add_signals(download_data("AAPL", "2018-01-01", "2024-01-01")))
+## print(add_signals(download_data("AAPL", "2018-01-01", "2024-01-01")))
 
-##Actual backtest
+## Actual backtest
 def backtest(df):
     df['Strategy_Return'] = df['Position'] * df['Return'] # Remember, the return could be negative when we have position
     df['Cum_Strategy'] = (1 + df['Strategy_Return']).cumprod() #cumprod is cumulative product. Ex: [1,2,3,4] = [1,2,6,24] 
@@ -37,9 +38,9 @@ def backtest(df):
     print(f"Max Drawdown: {max_drawdown:.2%}")
     
     return df
-##backtest(add_signals(download_data("AAPL", "2018-01-01", "2024-01-01")))
+## backtest(add_signals(download_data("AAPL", "2018-01-01", "2024-01-01")))
 
-##Plotting
+## Plotting
 def plot_returns(df, ticker):
     plt.figure(figsize=(12, 6))
     plt.plot(df['Cum_Buy_Hold'], label='Buy & Hold')
@@ -49,15 +50,22 @@ def plot_returns(df, ticker):
     plt.show()
 
 
-# -------------------------------------
-# 6. Main execution
-# -------------------------------------
+## Main execution
 def main():
-    ticker = input("Enter a stock ticker: ").upper()
-    start = "2019-01-01"
-    end = "2024-01-01"
-    
+    while True:
+        try:
+            ticker = input("Enter a stock ticker: ").strip().upper() #the strip and upper makes input cleaner btw
+            if not ticker.isalpha():
+                raise ValueError("Ticker needs to be letter!")
+            start = input("Enter a start date (YYYY-MM-DD):").strip() #Don't need upper, it's date
+            end = input("Enter a end date (YYYY-MM-DD):").strip() #You too
+            break
+        except ValueError:
+            print("Invalid input! Try again!") #Lowk might not need this
     df = download_data(ticker, start, end)
+    if df.empty:
+        print("No data have found for the given ticker.")
+        return
     df = add_signals(df)
     df = backtest(df)   
     plot_returns(df, ticker)
